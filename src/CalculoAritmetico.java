bb0b43125e61838a10accee92d210a522cbd3e98
Matheus Ferreira <matheusfer.dev@gmail.com>
2023-11-15 09:00:29 -0300

    Initial commit

---
+++ src/CalculoAritmetico.java
@@ -0,0 +1,15 @@
+public class CalculoAritmetico {
+
+    public static void main(String args[]) {
+
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int num4 = 40;
+
+        int media = (num1 + num2 + num3 + num4) / 4;
+
+        System.out.println(media);
+    }
+
+}
